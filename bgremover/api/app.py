d3835b671cb372e740320d1d19d195ff392d1424
artistacademyphilippines <133652535+artistacademyphilippines@users.noreply.github.com>
2025-02-17 19:52:33 +0800

    Add files via upload

--- bgremover/api/app.py
+++ bgremover/api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS  # We'll add this to handle CORS
 
 app = Flask(__name__)
@@ -6,11 +6,13 @@ CORS(app)  # This will enable CORS for all routes
 
 @app.route('/api', methods=['POST'])
 def process_text():
-    data = request.json
-    input_text = data.get('text', '')
-    # Process the input_text here. For this example, we'll just reverse it.
-    result = input_text[::-1]
-    return jsonify({'result': result})
+    data = request.data.decode('utf-8')
+
+    if data:
+        print(f"Received flask message: {data}")
+        return f"Received: {data}", 200  # Respond with a plain text message
+    else:
+        return "No message provided", 400
 
 if __name__ == '__main__':
     app.run()
