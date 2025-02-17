ab51ad58cd7a3a995eafb3dc82a4fe28145d23e8
artistacademyphilippines <133652535+artistacademyphilippines@users.noreply.github.com>
2025-02-18 01:01:20 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,18 +1,24 @@
-from flask import Flask, request
+from flask import Flask, render_template
 from flask_cors import CORS  # We'll add this to handle CORS
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
-@app.route('/api', methods=['POST'])
-def process_text():
-    data = request.data.decode('utf-8')
+#@app.route('/api', methods=['POST'])
+@app.route('/api')
 
-    if data:
-        return data
-    else:
-        # Return an error as plain text
-        return 400
+
+#def process_text():
+#    data = request.data.decode('utf-8')
+
+#    if data:
+#        return data
+#    else:
+#        # Return an error as plain text
+#        return 400
+
+def index():
+    print('bimbi')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
