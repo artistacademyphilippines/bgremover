dadbccf80c4a83748c264ef275089b17c36744d3
artistacademyphilippines <133652535+artistacademyphilippines@users.noreply.github.com>
2025-02-17 21:21:40 +0800

    Add files via upload

--- bgremover/api/app.py
+++ bgremover/api/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS  # We'll add this to handle CORS
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
-@app.route('/api', methods=['POST'])
+@app.route('/api', methods=['GET', 'POST'])
 def process_text():
     data = request.data.decode('utf-8')
 
