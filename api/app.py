98645de0a1cd167aa91e2bec9e8dd372fbd06de3
artistacademyphilippines <133652535+artistacademyphilippines@users.noreply.github.com>
2025-02-17 23:34:04 +0800

    Add files via upload

--- api/app.py
+++ api/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS  # We'll add this to handle CORS
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
-@app.route('/api', methods=['GET', 'POST'])
+@app.route('/api', methods=['POST'])
 def process_text():
     data = request.data.decode('utf-8')
 
