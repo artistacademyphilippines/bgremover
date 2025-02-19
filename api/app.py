2185f1720b81c95dfea5ac60fdc59dcdc0110fb6
artistacademyphilippines <133652535+artistacademyphilippines@users.noreply.github.com>
2025-02-19 23:25:39 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,3 +8,6 @@ CORS(app)
 @app.route('/api')
 def index():
     return "Hello from Flask!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
