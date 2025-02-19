88ef02c0e9cf66eea29c8dc6aade8991bf2145ae
artistacademyphilippines <133652535+artistacademyphilippines@users.noreply.github.com>
2025-02-19 18:40:18 +0800

    Add files via upload

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from rembg import remove
 import base64
@@ -17,7 +17,8 @@ def index():
 def remove_background():
     try:
         # Get the base64 string from the request
-        base64_string = request.data.decode('utf-8')
+        data = request.json
+        base64_string = data['image']
 
         # Decode the base64 string to image
         img_data = base64.b64decode(base64_string.split(',')[1])
@@ -31,6 +32,6 @@ def remove_background():
         output.save(buffered, format="PNG")
         img_str = base64.b64encode(buffered.getvalue()).decode()
 
-        return f'data:image/png;base64,{img_str}'
+        return jsonify({'image': f'data:image/png;base64,{img_str}'})
     except Exception as e:
-        return str(e), 500
+        return jsonify({'error': str(e)}), 500
