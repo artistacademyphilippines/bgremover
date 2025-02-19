b2e9448a0847d80d9cc01c1570bceabd4c3f31d1
artistacademyphilippines <133652535+artistacademyphilippines@users.noreply.github.com>
2025-02-20 07:24:43 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -5,10 +5,6 @@ app = Flask(__name__)
 @app.route('/api')
 def index():
     return "Hello from Flask!"
-
-@app.route('/health', methods=['GET'])
-def health_check():
-    return 'OK', 200
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
