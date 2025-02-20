a7132cd531cb17dd7a87727252e53955bca1bbd6
artistacademyphilippines <133652535+artistacademyphilippines@users.noreply.github.com>
2025-02-20 21:18:08 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -11,15 +11,12 @@ CORS(app, origins=["http://127.0.0.1:5503", "https://artistacademyphilippines.gi
 @app.route('/', methods=['GET','POST'])
 
 def index():
-    return "Hello from Boss Ozy!"
-
-def remove_background():
     #try:
         # Get the base64 string from the request
         data = request.data.decode('utf-8')
         base64_string = data
 
-        return data
+        return "Hello from Boss Ozy!" + data
         '''
         # Decode the base64 string to image
         img_data = base64.b64decode(base64_string.split(',')[1])
