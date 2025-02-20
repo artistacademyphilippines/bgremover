00ef519139feb82cf7e64bbcca46fc4ca0106813
artistacademyphilippines <133652535+artistacademyphilippines@users.noreply.github.com>
2025-02-20 19:16:19 +0800

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,4 @@
 # gunicorn_config.py
 
-bind = "0.0.0.0:80"  # Bind to all available IPs on port 8000, adjust the port if needed
+bind = "0.0.0.0:5503"  # Bind to all available IPs on port 8000, adjust the port if needed
 workers = 4  # Number of worker processes (adjust based on your CPU cores)
