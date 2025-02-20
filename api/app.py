3a0aaf885e06d48538066bddb689f16d4456e2a7
artistacademyphilippines <133652535+artistacademyphilippines@users.noreply.github.com>
2025-02-20 20:33:00 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS
 from rembg import remove
 import base64
@@ -6,19 +6,18 @@ import io
 from PIL import Image
 
 app = Flask(__name__)
-CORS(app, origins=["http://127.0.0.1:5503"])
-    
-@app.route('/')
-@app.route('/api')
+CORS(app, origins=["http://127.0.0.1:5503", "https://artistacademyphilippines.github.io"])
+
+@app.route('/', methods=['GET','POST'])
+
 def index():
     return "Hello from Boss Ozy!"
 
-@app.route('/rembg', methods=['GET', 'POST'])
 def remove_background():
     try:
         # Get the base64 string from the request
-        data = request.json
-        base64_string = data['image']
+        data = request.data.decode('utf-8')
+        base64_string = data
 
         # Decode the base64 string to image
         img_data = base64.b64decode(base64_string.split(',')[1])
@@ -32,9 +31,9 @@ def remove_background():
         output.save(buffered, format="PNG")
         img_str = base64.b64encode(buffered.getvalue()).decode()
 
-        return jsonify({'image': f'data:image/png;base64,{img_str}'})
+        return f'data:image/png;base64,{img_str}'
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return {'error': str(e)}, 500
         
 if __name__ == '__main__':
     app.run()
